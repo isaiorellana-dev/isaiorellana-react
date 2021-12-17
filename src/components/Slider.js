d176d4fc8cac1728706a0163c3d9d541fec72f47
isai-raw <isai7orellana@gmailcom>
2021-12-17 10:22:16 -0600

    :white_check_mark: Logica manual del Slider Completada
    Agregada la 5 imagen al funcionamiento del slider y nuevo orden a las imgs

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -13,10 +13,10 @@ const Slider = () => {
   const [indexImg, setIndexImg] = React.useState(0);
 
   const next = () => {
-    indexImg === 3 ? setIndexImg(0) : setIndexImg(indexImg + 1);
+    indexImg === 4 ? setIndexImg(0) : setIndexImg(indexImg + 1);
   };
   const prev = () => {
-    indexImg === 0 ? setIndexImg(3) : setIndexImg(indexImg - 1);
+    indexImg === 0 ? setIndexImg(4) : setIndexImg(indexImg - 1);
   };
 
   // setTimeout(() => {
@@ -29,36 +29,39 @@ const Slider = () => {
       <div className="img-container">
         <img className={clasesImg[indexImg]} src={nelsi} />
         <img
-          className={clasesImg[indexImg === 3 ? 0 : indexImg + 1]}
+          className={clasesImg[indexImg === 4 ? 0 : indexImg + 1]}
           src={paperas}
         />
         <img
           className={
             clasesImg[
-              indexImg === 3
+              indexImg === 4
                 ? 1
-                : indexImg + 2 && indexImg === 2
+                : indexImg + 2 && indexImg === 3
                 ? 0
                 : indexImg + 2
             ]
           }
-          src={maquillaje}
+          src={fire}
         />
         <img
           className={
             clasesImg[
-              indexImg === 1
+              indexImg === 2
                 ? 0
-                : indexImg + 3 && indexImg === 2
-                ? 1
                 : indexImg + 3 && indexImg === 3
+                ? 1
+                : indexImg + 3 && indexImg === 4
                 ? 2
                 : indexImg + 3
             ]
           }
           src={dariel}
         />
-        <img className={clasesImg[4]} src={fire} />
+        <img
+          className={clasesImg[indexImg === 0 ? 4 : indexImg - 1]}
+          src={maquillaje}
+        />
       </div>
       <div className="arrowLeft" onClick={prev}>
         <img src={left} alt="left" />
