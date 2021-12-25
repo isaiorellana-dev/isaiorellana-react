a4484d07ffe3d55e218e6b24ef455acf05c23661
isai-raw <isai7orellana@gmailcom>
2021-12-25 17:05:15 -0600

    :art: Hook para el Slider y loading Skeletons
    La logica del slider la pase a un Hook y le puse loading skeletons a las imagenes del slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,79 +7,70 @@ import left from "@icons/left.svg";
 import right from "@icons/right.svg";
 import fire from "@img/fire.jpg";
 import "@styles/Slider.scss";
+import useSlider from "@hooks/useSlider";
+
 const Slider = () => {
-  const clasesImg = ["img-1", "img-2", "img-3", "img-4", "img-5"];
+  const { clasesImg, indexImg, setAutoplay, prev, next } = useSlider();
 
-  const [indexImg, setIndexImg] = React.useState(0);
-  const [autoplay, setAutoplay] = React.useState(true);
-
-  const prev = () => {
-    indexImg === 4 ? setIndexImg(0) : setIndexImg(indexImg + 1);
+  const [loading, setLoading] = React.useState(true);
+  window.onload = () => {
+    setLoading(false);
   };
-  function next() {
-    indexImg === 0 ? setIndexImg(4) : setIndexImg(indexImg - 1);
-    return indexImg;
-  }
-
-  let timer;
-  const updateTimer = () => {
-    timer = setInterval(() => {
-      next();
-    }, 3000);
-    if (autoplay === false) clearInterval(timer);
-  };
-
-  React.useEffect(() => {
-    updateTimer();
-
-    return () => clearInterval(timer);
-  }, [autoplay, indexImg]);
-
-  if (!autoplay) {
-    window.onscroll = () => {
-      setAutoplay(true);
-    };
-  }
-
+  console.log(loading);
   return (
     <div className="slider">
-      <div className="img-container">
-        <img className={clasesImg[indexImg]} src={nelsi} />
-        <img
-          className={clasesImg[indexImg === 4 ? 0 : indexImg + 1]}
-          src={paperas}
-        />
-        <img
-          className={
-            clasesImg[
-              indexImg === 4
-                ? 1
-                : indexImg + 2 && indexImg === 3
-                ? 0
-                : indexImg + 2
-            ]
-          }
-          src={fire}
-        />
-        <img
-          className={
-            clasesImg[
-              indexImg === 2
-                ? 0
-                : indexImg + 3 && indexImg === 3
-                ? 1
-                : indexImg + 3 && indexImg === 4
-                ? 2
-                : indexImg + 3
-            ]
-          }
-          src={dariel}
-        />
-        <img
-          className={clasesImg[indexImg === 0 ? 4 : indexImg - 1]}
-          src={maquillaje}
-        />
-      </div>
+      {loading && (
+        <div className="img-container">
+          <div className="img-2">
+            <div></div>
+          </div>
+          <div className="img-3">
+            <div></div>
+          </div>
+          <div className="img-4">
+            <div></div>
+          </div>
+        </div>
+      )}
+      {!loading && (
+        <div className="img-container">
+          <img className={clasesImg[indexImg]} src={nelsi} />
+          <img
+            className={clasesImg[indexImg === 4 ? 0 : indexImg + 1]}
+            src={paperas}
+          />
+          <img
+            className={
+              clasesImg[
+                indexImg === 4
+                  ? 1
+                  : indexImg + 2 && indexImg === 3
+                  ? 0
+                  : indexImg + 2
+              ]
+            }
+            src={fire}
+          />
+          <img
+            className={
+              clasesImg[
+                indexImg === 2
+                  ? 0
+                  : indexImg + 3 && indexImg === 3
+                  ? 1
+                  : indexImg + 3 && indexImg === 4
+                  ? 2
+                  : indexImg + 3
+              ]
+            }
+            src={dariel}
+          />
+          <img
+            className={clasesImg[indexImg === 0 ? 4 : indexImg - 1]}
+            src={maquillaje}
+          />
+        </div>
+      )}
       <div
         className="arrowLeft"
         onClick={() => {
